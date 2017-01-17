b010a8f43c827ce1fae76463bad7aef48cf49aca
wuyongxiang <810335911@qq.com>
2017-01-17 17:59:29 +0800

    no message

--- app/src/main/java/com/xiangzi/qq_list/BallView.java
+++ app/src/main/java/com/xiangzi/qq_list/BallView.java
@@ -1,5 +1,6 @@
 package com.xiangzi.qq_list;
 
+import java.util.List;
 import java.util.Random;
 
 import android.content.Context;
@@ -7,20 +8,56 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ListView;
 
 
-public class BallView extends ListView{
+public class BallView extends View {
+	int GROUND_LING = 0 ; //表示球在第几个坐标
 
-	public static int GROUND_LING;
-	public static final int UP_ZERO = 30;
-	public static final int DOWN_ZERO = 60;
-	Bitmap bitmap ;
-	Movable m;
+	public static final int UP_ZERO = 30; //向上最小速度，小于UP_ZERO表示升到最高处
+	public static final int DOWN_ZERO = 60; //小于DOWN_ZERO表示小球已落地
+	public static float g = 4000; //重力加速度
+
+	int startX = 0; //表情运动前的坐标
+	int startY = 0;
+
+	int currentX= -100;//表情实时运动坐标
+	int currentY= -100;
+
+	float startVX = 0f;//表情初始速度
+	float startVY = -(float)(1200);
+
+	float currentVX = 0f;//表情实时速度
+	float currentVY = 0f;
+
+	Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.huaji);
+	int r = bitmap.getHeight()/2;
+
+	float impactFactoryY = 0.5f;//衰减系数
+	float impactFactoryX = 0.1f;
+
+	double timeX;
+	double timeY;
+
+
+
+	List<Point> pointList;
 	Canvas canvas;
+
+	Bitmap wood = BitmapFactory.decodeResource(getResources(), R.drawable.wood);
+	BallThread bt = null;
+	boolean bFall = false;
+
+
+
 	public BallView(Context Activity) {
 		super(Activity);
+
 	}
 
 	public BallView(Context context, AttributeSet attrs) {
@@ -29,38 +66,110 @@ public class BallView extends ListView{
 	public BallView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
-	public void initMovables(){
-		initBitmaps(getResources());
-//		Random r = new Random();
-//		int index = r.nextInt(32);
-//		Bitmap tempBitmap = null;
-//		tempBitmap = bitmap;
-//		int startX = 0;
-//		int startY = 300;
-//		int finalX = 200;
-//
-//		float startVy = -(float)(1000);
-//		double t1 = -startVy/BallThread.g;
-//		GROUND_LING = 800;
-//		double s = (startVy*t1+0.5*BallThread.g*t1*t1);
-//		double t2 =Math.sqrt(((2*(GROUND_LING-startY+s))/BallThread.g));
-//		double t = t1+t2;
-//		float startVx = (float)((finalX-startX)/(t));
-//
-//		m = new Movable(startX, startY, startVx,startVy,tempBitmap.getWidth()/2, tempBitmap ,canvas);
+	public void initMovables(List<Point> pointList){
+		this.pointList =pointList;
 
-
-		canvas.drawBitmap(bitmap, 200, 200, null);
-		canvas.restore();
-		invalidate();
-	}
-
-	public void initBitmaps(Resources r){
-		bitmap = BitmapFactory.decodeResource(r, R.drawable.huaji);
+		GROUND_LING = 0;
+		startX = pointList.get(GROUND_LING).x;
+		startY = pointList.get(GROUND_LING).y;
+		bt = new BallThread();
+		bt.start();
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		this.canvas = canvas;
+		canvas.drawBitmap(bitmap, currentX, currentY, null);
 	}
+	public class BallThread extends Thread {
+		boolean flag = false;
+		double current;
+		public BallThread(){
+			timeX = System.nanoTime();
+			this.flag = true;
+		}
+
+		public void run(){
+			while(GROUND_LING<pointList.size()-1){
+				flag = true;
+				startVY = -(float)(1200);
+				double t1 = -startVY/g;
+				double s = (startVY*t1+0.5*g*t1*t1);
+				double t2 =Math.sqrt(((2*(pointList.get(GROUND_LING+1).y-startY+(Math.abs(s))))/g));
+				double t = t1+t2;
+				startVX = (float)((pointList.get(GROUND_LING+1).x-startX)/(t));
+				currentVX = startVX;
+				currentX = startX;
+				currentY = startY;
+				while(flag){
+					postInvalidate();
+					current = System.nanoTime();
+					double timeSpanX = (double)((current -  timeX)/1000/1000/1000);
+					currentX = (int)(startX +  currentVX * timeSpanX);
+					if( bFall){
+						double timeSpanY = (double)((current -  timeY)/1000/1000/1000);
+						currentY = (int)( startY +  startVY * timeSpanY + timeSpanY * timeSpanY * g/2);
+						currentVY = (float)( startVY + g * timeSpanY);
+						if( startVY < 0 && Math.abs( currentVY) <= BallView.UP_ZERO){
+							timeY = System.nanoTime();
+							currentVY = 0;
+							startVY = 0;
+							startY =  currentY;
+						}
+						if( currentY +  r*2 >= pointList.get(GROUND_LING+1).y &&  currentVY > 0){
+							currentVX =  currentVX * ( impactFactoryX);
+							currentVY = 0 -  currentVY * ( impactFactoryY);
+
+							if(Math.abs( currentVY) < BallView.DOWN_ZERO){
+								try{
+									Thread.sleep(100);
+								}catch(Exception e){
+									e.printStackTrace();
+								}
+								this.flag = false;
+								System.out.println("线程结束"+GROUND_LING);
+								GROUND_LING++;
+							}else{
+								startX = (int) currentX;
+								timeX = System.nanoTime();
+								startY =  currentY;
+								timeY = System.nanoTime();
+								startVY =  currentVY;
+							}
+						}
+					}else {
+						if( currentVY == 0 ||  currentVY > 0){
+							timeY = System.nanoTime();
+							bFall = true;
+						}else{
+							currentVY = (int)( startVY + g * timeSpanX);
+							currentY = (int)( currentY +  startVY * timeSpanX + timeSpanX * timeSpanX * g/2);
+						}
+
+
+					}
+				}
+			}
+
+		}
+	}
+//	Handler mHandler = new Handler() {
+//		public void handleMessage(Message msg) {
+//			int now = msg.what;
+//			if(now<pointList.size()-1){
+//				double t1 = -startVY/g;
+//				double s = (startVY*t1+0.5*g*t1*t1);
+//				double t2 =Math.sqrt(((2*(pointList.get(now+1).y-pointList.get(now).y+(Math.abs(s))))/g));
+//				double t = t1+t2;
+//				startVX = (float)((pointList.get(now+1).x-startX)/(t));
+//				currentVX = startVX;
+//				GROUND_LING = now+1;
+//				bt = new BallThread();
+//				bt.start();
+//			}
+//
+//
+//
+//		};
+//	};
 }
--- app/src/main/java/com/xiangzi/qq_list/MainActivity.java
+++ app/src/main/java/com/xiangzi/qq_list/MainActivity.java
@@ -1,7 +1,12 @@
 package com.xiangzi.qq_list;
 
+import android.graphics.Point;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +19,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
-	BallView list;
+	static BallView list;
 	ChatAdapter chatAdapter ;
 	List<Chat> chatlist = new ArrayList<Chat>();
+	List<Point> pointList = new ArrayList<Point>();
 	Button btn ;
 	String[] s = {"苍茫的天涯是我的爱" ,
 			"绵绵的青山脚下花正开" ,
@@ -32,31 +38,45 @@ public class MainActivity extends Activity {
 			"悠悠的唱着 最炫的民族风",
 			"让爱卷走所有的尘埃",
 			"（我知道）"};
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		list=(BallView) findViewById(R.id.list);
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		int x= (int)(displayMetrics.widthPixels);
+		pointList.add(new Point(120 , 50));
+		pointList.add(new Point(x - 140 ,250));
+		pointList.add(new Point(x - 180 ,450));
+		pointList.add(new Point(100 , 650));
+		pointList.add(new Point(x - 140 , 800));
+		pointList.add(new Point(x - 160 , (int)(displayMetrics.heightPixels)+40));
 
-		for (int i = 0;i<14;i++){
-			Chat chat = new Chat();
-			chat.chatfrom = (int)(Math.random()*(2));
-			chat.text = s[i];
-			chatlist.add(chat);
-		}
-		chatAdapter = new ChatAdapter(this , chatlist );
-		list.setAdapter(chatAdapter);
+//		for (int i = 0;i<14;i++){
+//			Chat chat = new Chat();
+//			chat.chatfrom = (int)(Math.random()*(2));
+//			chat.text = s[i];
+//			chatlist.add(chat);
+//		}
+//		chatAdapter = new ChatAdapter(this , chatlist );
+//		list.setAdapter(chatAdapter);
 		btn = (Button)findViewById(R.id.button);
 		btn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				int firstPosition = list.getFirstVisiblePosition();
-				int lastPosition = list.getLastVisiblePosition();
-				System.out.println("position::"+firstPosition);
-				View view = list.getChildAt(lastPosition);
-				int height  = view.getTop();
-				System.out.println("height::"+height);
-				list.initMovables();
+//				int firstPosition = list.getFirstVisiblePosition();
+//				int lastPosition = list.getLastVisiblePosition();
+//				System.out.println("position::"+firstPosition);
+//				View view = list.getChildAt(lastPosition);
+//				int height  = view.getTop();
+//				System.out.println("height::"+height);
+
+				list.initMovables(pointList);
+
+
 			}
 		});
 	}
